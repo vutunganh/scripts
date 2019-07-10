9da67d34634902c501e6808710ec72dc5316d77e
Tung Anh Vu <vutungan@fit.cvut.cz>
2019-07-10 11:32:49 +0200

    New menu link and HTTP::Tiny

--- pod-loubim/pod-loubim.pl
+++ pod-loubim/pod-loubim.pl
@@ -1,9 +1,11 @@
-#!/usr/bin/perl -CS
+#!/usr/bin/env perl
 
 use strict;
 use warnings;
 use utf8;
-use LWP::UserAgent;
+use HTTP::Tiny;
+binmode(STDOUT, ":utf8");
+binmode(STDERR, ":utf8");
 
 my $vflag = 0; # verbose
 my $lflag = 0; # lowercase menu?
@@ -12,18 +14,16 @@ my $rflag = 0; # print restaurant name?
 sub handle_cli_args {
   while ($#ARGV >= 0) {
     if ($ARGV[0] eq "-h" || $ARGV[0] eq "--help") {
-      print STDERR
-      "Displays Pod Loubim's daily menu.\n",
-      "Usage: pod-loubim.pl [-h|--help]\n",
-      "       pod-loubim.pl [-v|--verbose] [-r|--print-restaurant-name]",
-      " [-l|--lower-case]\n\n",
-      "  -h|--help                     prints this help message\n",
-      "  -v|--verbose                  enables verbose mode\n",
-      "  -t|--print-restaurant-name    whether or not to display '",
-      " Pod Loubim' header when printing the menu\n",
-      "  -l|--lower-case               whether to convert the result to lower",
-      " case before printing\n";
-      exit 0;
+      die <<"END"
+Displays Pod Loubim's daily menu.
+Usage: pod-loubim.pl [-h|--help]
+       pod-loubim.pl [-v|--verbose] [-r|--print-restaurant-name] [-l|--lower-case]
+
+  -h|--help                     prints this help message
+  -v|--verbose                  enables verbose mode
+  -t|--print-restaurant-name    whether or not to display 'Pod Loubim' header when printing the menu
+  -l|--lower-case               whether to convert the result to lowercase before printing
+END
     } elsif ($ARGV[0] eq "-v" || $ARGV[0] eq "--verbose") {
       $vflag = 1;
       print STDERR "Verbose mode on.\n";
@@ -53,21 +53,18 @@ sub isweekend {
 
 sub fetch_site {
   my %links = (
-    weekday => 'http://www.podloubim.com/menu/',
+    weekday => 'http://www.podloubim.com/tydenni-menu/',
     weekend => 'http://www.podloubim.com/vikendove-menu/'
   );
 
   my $link = isweekend() ? $links{"weekend"} : $links{"weekday"};
 
   print STDERR "Fetching '$link'.\n" if $vflag;
-  my $ua = LWP::UserAgent->new;
-  $ua->timeout(10);
-  $ua->env_proxy;
 
-  my $response = $ua->get($link);
-  $response->is_success or die "Couldn't fetch menu.\n";
+  my $response = HTTP::Tiny->new->get($link);
+  $response->{success} or die "Couldn't fetch menu.\n";
   print STDERR "Successfully fetched menu.\n" if $vflag;
-  return $response->decoded_content;
+  return $response->{content};
 }
 
 sub obtain_menu {
